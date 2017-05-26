0e54d4b7a5245a5e75f905b26a064b8cfe6543ed
Manpreet <manpreet.singh@gaussnetworks.com>
2017-05-26 18:38:35 +0530

    Es6 basic

--- ES6-learn/test.js
+++ ES6-learn/test.js
@@ -8,3 +8,24 @@ console.log(no);
 let no = 10; 
 //let no = 20; 
 console.log(no); // error no has already been declared
+
+/*function add(a, b = 1) { 
+   return a+b; 
+} 
+console.log(add(4))
+
+function add(a, b = 1) { 
+   return a + b; 
+} 
+console.log(add(4,2))
+
+
+*/
+
+// reset parameters
+function fun1(...params) { 
+   console.log(params.length); 
+}
+fun1();  
+fun1(5); 
+fun1(5, 6, 7); 
