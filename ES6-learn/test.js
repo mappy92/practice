65d173cb0e6c09089b274534c139b9c3fe70ba75
Manpreet <manpreet.singh@gaussnetworks.com>
2017-05-26 17:21:04 +0530

    ES6 initial commit

---
+++ ES6-learn/test.js
@@ -0,0 +1,10 @@
+var mesaage ="hello es6";
+console.log(mesaage);
+
+var no = 10; 
+var no = 20; 
+console.log(no);
+
+let no = 10; 
+//let no = 20; 
+console.log(no); // error no has already been declared
