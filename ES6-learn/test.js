c40292d2dab494e125afb258041857b5d8714be4
mappy92 <manpreet4114@gmail.com>
2017-06-03 09:45:42 +0530

    es6 commit

--- ES6-learn/test.js
+++ ES6-learn/test.js
@@ -5,7 +5,7 @@ var no = 10;
 var no = 20; 
 console.log(no);
 
-let no = 10; 
+//let no = 10; 
 //let no = 20; 
 console.log(no); // error no has already been declared
 
@@ -22,10 +22,23 @@ console.log(add(4,2))
 
 */
 
+// default parameters
+
+function add(a, b = 1) { 
+   return a+b; 
+} 
+console.log(add(4));
+ 
+console.log(add(4,2));
+
 // reset parameters
 function fun1(...params) { 
    console.log(params.length); 
 }
 fun1();  
 fun1(5); 
-fun1(5, 6, 7); 
+fun1(5, 6, 7);
+
+
+
+
