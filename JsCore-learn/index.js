2e52bd12c6982c47083498702ec7284ec96fd88c
Manpreet <manpreet.singh@gaussnetworks.com>
2017-06-16 19:03:34 +0530

    office commit

--- JsCore-learn/index.js
+++ JsCore-learn/index.js
@@ -1,1 +1,23 @@
+var a=1;
+var b=2;
+var c=3;
+var d=4;
+var e = 1000;
+console.log(a);
+
+setTimeout(function(){
+   console.log(b); 
+},0);
+
+setTimeout(function(){
+   console.log(c); 
+},10);
+
+while(e){
+   console.log(e);
+   e--;
+}
+console.log(d);
+
+
 
